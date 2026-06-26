9fdac515ba572905d3186aedeae2135a98011751
Chetan Wani <02ckwani@gmail.com>
2026-06-26 18:11:32 +0530

    Automatically create admin user

--- build.sh
+++ build.sh
@@ -5,3 +5,19 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 
 python manage.py migrate
+
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+username = "admin"
+password = "admin123"
+email = "admin@example.com"
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print("Superuser created.")
+else:
+    print("Superuser already exists.")
+EOF
