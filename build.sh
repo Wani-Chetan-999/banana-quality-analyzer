08b56a49d27aa70ca23a4c0eeb3670b33c5831fc
Chetan Wani <02ckwani@gmail.com>
2026-06-26 18:14:04 +0530

    Auto create superuser

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -o errexit
 
 pip install -r requirements.txt
 
@@ -11,12 +12,12 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-username = "admin"
-password = "admin123"
-email = "admin@example.com"
-
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username, email, password)
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser(
+        username="admin",
+        email="admin@example.com",
+        password="admin123"
+    )
     print("Superuser created.")
 else:
     print("Superuser already exists.")
